fc2c75dd6c17c01ebf065830f4ed5cd3af739ad7
Alan Chapman <brochapman@gmail.com>
2022-02-03 19:45:21 -0700

    initial dev commit

---
+++ download-sid.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -eu
+
+# Arch Linux Install Script (alis) installs unattended, automated
+# and customized Arch Linux system.
+# Copyright (C) 2022 picodotdev
+
+GITHUB_USER="picodotdev"
+BRANCH="sid"
+
+while getopts "u:" arg; do
+  case ${arg} in
+    u)
+      GITHUB_USER=${OPTARG}
+      ;;
+    ?)
+      echo "Invalid option: -${OPTARG}."
+      exit 1
+      ;;
+  esac
+done
+
+set -o xtrace
+curl -sL -o "alis-$BRANCH.zip" https://github.com/$GITHUB_USER/alis/archive/refs/heads/$BRANCH.zip
+bsdtar -x -f "alis-$BRANCH.zip"
+cp -R alis-$BRANCH/*.sh alis-$BRANCH/*.conf alis-$BRANCH/files/ ./
+chmod +x *.sh
+
